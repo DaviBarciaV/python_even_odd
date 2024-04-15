d8bd2f32712f1955fcce2966757e4ed4046dd2cd
DaviBarciaV <164547786+DaviBarciaV@users.noreply.github.com>
2024-04-15 09:49:58 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+numero = int(input("Digite um número (0 para sair): "))
+        
+if numero == 0:
+        print("Foi digitado o zero. Saindo...")
+elif numero %2 == 1:
+        print(f"O número {numero} é ímpar")
+else:
+        print(f"O número {numero} é par.")        
